043ec875a86a252236f207a17118358c55a256d8
maicon3000 <maicon3000@hotmail.com>
2020-02-21 04:31:55 -0300

    Melhoramento do código e comando for() para n 'olá'

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.printf("Digite 1 para saudação ou outra tecla para sair: ");
+
+		System.out.println("Olá mundo! Olá pra você!");
+		System.out.printf("Entre com um número: ");
 		int n = sc.nextInt();
-		if(n == 1) {
-		System.out.println("Olá mundo!");
+		if (n <= 1) {
+			System.out.println("Olá novamente! Rs");
 		} else {
+			for (int i = 2; i <= n; i++) {
+				System.out.println("Olá " + i + " vezes");
+			}
 		}
-		
 		sc.close();
 	}
 
